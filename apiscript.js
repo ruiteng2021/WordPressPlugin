540a39699305fa5a99a26bcc8618879185dbe3e0
ruiteng2021 <tengrui13@gmail.com>
2021-04-22 23:45:52 -0400

    add moreoption

--- apiscript.js
+++ apiscript.js
@@ -15,20 +15,21 @@ fetchApiData().then(data => {
     let categories = [];
     let html = '';
     html += '<tr>';
-    html += '<td>Id</td>';
-    html += '<td>Name</td>';
+    html += '<td>' + data.data["name"] + '</td>';
+    html += '<td>Product</td>';
     html += '<td>THC%</td>';
     html += '<td>CBD%</td>';
     html += '<td>Price</td>';
     html += '<td>Category';
     html += '<form method="POST">';
     html += '<select name="Category" id= "cat" onChange= "UpdateTableCategory()">';
-    html +=  '<option value="selected enabled" name="all">All</option>';
+    html += '<option value="selected enabled" name="all">All</option>';
 
+    // remove duplicate
     for ( let i in products) {
         categories[i] =  products[i].category;
     }
-    let category = [...new Set(categories)];;
+    let category = [...new Set(categories)];
     console.log(category);
 
     for ( let i in category) {
@@ -43,9 +44,14 @@ fetchApiData().then(data => {
     for ( let i in products) {
         // console.log(products[i].id);
         // console.log(products[i].name);
+        console.log(products[i].image_url);
         html +=  '<tr>';
-        html += '<td>' + products[i].id + '</td>';
-        html += '<td>' + products[i].name + '</td>';
+        html += '<td><img src="' + products[i].image_url + '" alt="" height="200" width="200">' + '</td>';
+        html += '<td>' + products[i].name + '<br>' + 
+                '<p>' + products[i].price_oz_eighth + ' per 1/8 oz</p>' + 
+                '<p>' + products[i].price_oz_fourth + ' per 1/4 oz</p>' + 
+                '<p>' + products[i].price_oz_half + ' per 1/2 oz</p>' + 
+                '<p>' + products[i].price_oz + ' per 1 oz</p>' + '</td>';
         html += '<td>' + products[i].thc + '</td>';
         html += '<td>' + products[i].cbd + '</td>';
         html += '<td>' + products[i].price + '</td>';
@@ -69,8 +75,12 @@ function UpdateTableCategory()
         if ( value.localeCompare(products[i].category) == 0 ) 
         {
             html += '<tr>';
-            html += '<td>' + products[i].id + '</td>';
-            html += '<td>' + products[i].name + '</td>';
+            html += '<td><img src="' + products[i].image_url + '" alt="" height="200" width="200">' + '</td>';
+            html += '<td>' + products[i].name + '<br>' + 
+                '<p>' + products[i].price_oz_eighth + ' per 1/8 oz</p>' + 
+                '<p>' + products[i].price_oz_fourth + ' per 1/4 oz</p>' + 
+                '<p>' + products[i].price_oz_half + ' per 1/2 oz</p>' + 
+                '<p>' + products[i].price_oz + ' per 1 oz</p>' + '</td>';
             html += '<td>' + products[i].thc + '</td>';
             html += '<td>' + products[i].cbd + '</td>';
             html += '<td>' + products[i].price + '</td>';
@@ -80,8 +90,12 @@ function UpdateTableCategory()
         if ( value.localeCompare('selected enabled') == 0 ) 
         {
             html += '<tr>';
-            html += '<td>' + products[i].id + '</td>';
-            html += '<td>' + products[i].name + '</td>';
+            html += '<td><img src="' + products[i].image_url + '" alt="" height="200" width="200">' + '</td>';
+            html += '<td>' + products[i].name + '<br>' + 
+                '<p>' + products[i].price_oz_eighth + ' per 1/8 oz</p>' + 
+                '<p>' + products[i].price_oz_fourth + ' per 1/4 oz</p>' + 
+                '<p>' + products[i].price_oz_half + ' per 1/2 oz</p>' + 
+                '<p>' + products[i].price_oz + ' per 1 oz</p>' + '</td>';
             html += '<td>' + products[i].thc + '</td>';
             html += '<td>' + products[i].cbd + '</td>';
             html += '<td>' + products[i].price + '</td>';
